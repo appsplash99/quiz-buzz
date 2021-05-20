961b187ff589ebc2216678bc03bdbc778c54cce3
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-05-20 21:20:29 +0530

    initialised: base setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import './App.css';
 
-export const App: React.FC () {
-  return (
-      <>Working App</>
-  );
-}
+export const App = () => {
+  return <>Working App</>;
+};
 
 export default App;
